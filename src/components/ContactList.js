2cac0cc0af030a78d7dda6b6fb2b325e1cc5d2eb
l1nes8 <egor1glavatyje@gmail.com>
2023-10-29 01:38:39 +0300

    update

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import css from 'form.module.css';
 
 export default function ContactList({ contacts, onDeleteContact }) {
   return (
     <div>
       <ul>
         {contacts.map(contact => (
-          <li key={contact.id}>
+          <li className={css.item} key={contact.id}>
             {contact.name}: {contact.number}
             <button onClick={() => onDeleteContact(contact.id)}>Delete</button>
           </li>
